8ef7a19dc0489ffb0749249cfb79fa5f7e1e6e41
Matt Karl <matt@mattkarl.com>
2014-01-26 22:40:31 -0500

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Engine",
+        "Lexer",
+        "Parser",
+        "ParserError",
+        "ProfilerLabel"
+    ],
+    "modules": [
+        "Canteen_Parser"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen\\Parser",
+            "name": "Canteen_Parser"
+        }
+    ]
+} };
+});
